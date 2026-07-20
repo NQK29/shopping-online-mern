4d58cfc6f4c59a29166eaef125ef320152df1362
NQK <ngoquockhanh741@gmail.com>
2026-07-20 13:16:31 +0700

    fix: resolve redundant git merge conflict markers

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -17,53 +16,18 @@ app.get('/hello', (req, res) => {
 app.use('/api/admin', require('./api/admin.js'));
 app.use('/api/customer', require('./api/customer.js'));
 
+// Serve frontend builds
 app.use('/admin', express.static(path.resolve(__dirname, '../client-admin/build')));
 app.get('admin/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client-admin/build', 'index.html'))
 });
+
 app.use('/', express.static(path.resolve(__dirname, '../client-customer/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client-customer/build', 'index.html'));
 });
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
-
-
-
-=======
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Middlewares
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
-
-// APIs
-app.get('/hello', (req, res) => {
-  res.json({ message: 'Hello from server!' });
-});
-app.use('/api/admin', require('./api/admin.js'));
-app.use('/api/customer', require('./api/customer.js'));
-
-app.use('/admin', express.static(path.resolve(__dirname, '../client-admin/build')));
-app.get('admin/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client-admin/build', 'index.html'))
-});
-app.use('/', express.static(path.resolve(__dirname, '../client-customer/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client-customer/build', 'index.html'));
-});
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
-
-
-
->>>>>>> 38d823e07c166169e21a1640e8edd9b8fb0436fe
